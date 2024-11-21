ea54cbb036ba14750e1b0f2206ee463b7649d73c
GalloSimone <gallosimonecz@gmail.com>
2024-11-21 16:16:53 +0100

    milestone 1 completata

---
+++ controllers/postscontroller.js
@@ -0,0 +1,30 @@
+//INDEX
+function index (req,res) {
+    res.json({message:"lista dei post",})
+}
+
+//SHOW
+function show (req,res) {
+    const id = parseInt(req.params.id);
+    res.json( ` post ${id}` )
+}
+
+//STORE
+function store (req,res){
+    res.json("creazione nuovo post")
+}
+
+//UPDATE
+function update (req,res){
+    const id = parseInt (req.params.id);
+    res.json(`aggiornamento del post ${id}`)
+}
+//DESTROY
+function destroy (req,res){
+    const id = parseInt (req.params.id);
+    res.json(`eliminazione del post ${id}`)
+}
+
+
+module.exports = {index,show,store,update,destroy};
+
--- routers/posts.js
+++ routers/posts.js
@@ -2,44 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 const posts =require("../data/posts.js")
-            //INDEX
-    router.get('/',(req, res)=> {
-
-            res.json({message: "POST", noticeBoard, noticeBoardCount: noticeBoard.length})
-            });
-
-
-            //SHOW 
-            router.get("/:id", (req,res)=>{
-                const {id} = req.params;
-                res.json( ` post ${id}` )
-            });
-
-            //STORE
-            router.post("/", (req,res)=>{
-             res.json( "creazione nuovo post" )
-
-            });
-
-            //UPDATE
-            router.put("/:id", (req,res)=>{
-                const {id}=req.params;
-                res.send(`aggiornamento del post ${id}`)
-            }) 
-            
-            //DESTROY
-            router.delete('/:id', (req,res)=>{
-                const {id} = req.params;
-                res.send(`eliminazione del post ${id}`)
-
-            })
 
 
 
+const postsController = require("../controllers/postscontroller.js")
 
 
-
-
+ //INDEX
+     router.get('/',postsController.index);
+ //SHOW 
+     router.get("/:id",postsController.show);
+ //STORE
+ router.post("/",postsController.store);
+ //UPDATE
+router.put("/:id",postsController.update) 
+//DESTROY
+ router.delete('/:id',postsController.destroy)
 
 
 module.exports = router;
